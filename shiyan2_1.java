53019aa682dd6db3a15a37f959059673c3b9761d
TianXing Xue <76930817+veritas0518@users.noreply.github.com>
2021-06-12 13:55:30 +0800

    Find the datas' max value, mini valueaverage value
    【问题描述】编写程序，先输入整数n，再输入n个整数，然后输出这个n整数的最大值、最小值，去掉最大值与最小值后剩余数据的平均值。输入输出格式如样例所示，输出平均值时保留6位小数。
    【样例输入1】10
    1 2 3 9 4 6 0 5 8 7
    【样例输出1】
    max = 9
    min = 0
    ave = 4.500000
    
    【样例输入2】8
    3 2 5 7 9 2 1 4
    【样例输出2】
    max = 9
    min = 1
    ave = 3.833333

---
+++ shiyan2_1.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.*;
+public class shiyan2_1 {
+    public static void main(String[] args)  {
+        Scanner s =new Scanner(System.in);
+        int max=0,min=0;
+        int n;
+        n=s.nextInt();
+        double ave,sum=0;
+        int[]a;
+        a = new int[n];
+
+        for(int i=0;i<n;i++) {
+            a[i] = s.nextInt();
+            sum=sum+a[i];
+        }
+        Arrays.sort(a);
+        max=a[n-1];
+        min=a[0];
+
+        sum=sum-max-min;
+        ave=sum/(n-2);
+        System.out.printf("max = %d\n",max);
+        System.out.printf("min = %d\n",min);
+        System.out.printf("ave = %.6f",ave);
+
+    }
+
+}
+
+
+
